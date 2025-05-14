9d387246c0a5468dead5b72cd74ceb155774e62c
George Stavrinos <gstavrinos@protonmail.com>
2025-05-14 14:49:22 +0300

    Added an extra check pose function that does not need the Costmap2DROS object
    This is useful for the new hack on the tracking planner that requires access to the global costmap

--- mbf_exposed_services/include/mbf_exposed_services/check_pose.h
+++ mbf_exposed_services/include/mbf_exposed_services/check_pose.h
@@ -4,17 +4,19 @@
 #include "mbf_costmap_nav/footprint_helper.h"
 #include "mbf_costmap_nav/costmap_navigation_server.h"
 
-
 namespace mbf_exposed_services
 {
 
-  class CheckPose 
-  {
-  public:
-    CheckPose();
-    ~CheckPose();
+class CheckPose
+{
+public:
+  CheckPose();
+  ~CheckPose();
 
-    bool check_pose_cost(costmap_2d::Costmap2DROS* costmap, mbf_msgs::CheckPose::Request &request, mbf_msgs::CheckPose::Response &response);
+  bool check_pose_cost_from_costmap_and_footprint(costmap_2d::Costmap2D*, std::vector<geometry_msgs::Point>,
+                                                  mbf_msgs::CheckPose::Request&, mbf_msgs::CheckPose::Response&);
 
-  };
+  bool check_pose_cost(costmap_2d::Costmap2DROS* costmap, mbf_msgs::CheckPose::Request& request,
+                       mbf_msgs::CheckPose::Response& response);
 };
+};  // namespace mbf_exposed_services
--- mbf_exposed_services/src/check_pose.cpp
+++ mbf_exposed_services/src/check_pose.cpp
@@ -4,89 +4,97 @@
 namespace mbf_exposed_services
 {
 
-  CheckPose::CheckPose() = default;
+CheckPose::CheckPose() = default;
 
-  CheckPose::~CheckPose() = default;
+CheckPose::~CheckPose() = default;
 
-  bool CheckPose::check_pose_cost(costmap_2d::Costmap2DROS* costmap, mbf_msgs::CheckPose::Request &request, mbf_msgs::CheckPose::Response &response)
+bool CheckPose::check_pose_cost_from_costmap_and_footprint(costmap_2d::Costmap2D* costmap_2d,
+                                                           std::vector<geometry_msgs::Point> footprint,
+                                                           mbf_msgs::CheckPose::Request& request,
+                                                           mbf_msgs::CheckPose::Response& response)
+{
+  double x = request.pose.pose.position.x;
+  double y = request.pose.pose.position.y;
+  double yaw = tf::getYaw(request.pose.pose.orientation);
+  costmap_2d::padFootprint(footprint, request.safety_dist);
+
+  // use footprint helper to get all the cells totally or partially within footprint polygon
+  std::vector<mbf_costmap_nav::Cell> footprint_cells =
+      mbf_costmap_nav::FootprintHelper::getFootprintCells(x, y, yaw, footprint, *costmap_2d, true);
+  response.state = mbf_msgs::CheckPose::Response::FREE;
+  if (footprint_cells.empty())
   {
-    double x = request.pose.pose.position.x;
-    double y = request.pose.pose.position.y;
-    double yaw = tf::getYaw(request.pose.pose.orientation);
+    // no cells within footprint polygon must mean that robot is at least partly outside of the map
+    response.state = std::max(response.state, static_cast<uint8_t>(mbf_msgs::CheckPose::Response::OUTSIDE));
+  }
+  else
+  {
+    // lock costmap so content doesn't change while adding cell costs
+    boost::unique_lock<costmap_2d::Costmap2D::mutex_t> lock(*(costmap_2d->getMutex()));
 
-    // ensure costmap is active so cost reflects latest sensor readings
-    // costmap->checkActivate();
-
-    // pad raw footprint to the requested safety distance; note that we discard footprint_padding parameter effect
-    std::vector<geometry_msgs::Point> footprint = costmap->getUnpaddedRobotFootprint();
-    costmap_2d::padFootprint(footprint, request.safety_dist);
-
-    // use footprint helper to get all the cells totally or partially within footprint polygon
-    std::vector<mbf_costmap_nav::Cell> footprint_cells =
-      mbf_costmap_nav::FootprintHelper::getFootprintCells(x, y, yaw, footprint, *costmap->getCostmap(), true);
-    response.state = mbf_msgs::CheckPose::Response::FREE;
-    if (footprint_cells.empty())
+    // integrate the cost of all cells; state value precedence is UNKNOWN > LETHAL > INSCRIBED > FREE
+    for (int i = 0; i < footprint_cells.size(); ++i)
     {
-      // no cells within footprint polygon must mean that robot is at least partly outside of the map
-      response.state = std::max(response.state, static_cast<uint8_t>(mbf_msgs::CheckPose::Response::OUTSIDE));
-    }
-    else
-    {
-      // lock costmap so content doesn't change while adding cell costs
-      boost::unique_lock<costmap_2d::Costmap2D::mutex_t> lock(*(costmap->getCostmap()->getMutex()));
-
-      // integrate the cost of all cells; state value precedence is UNKNOWN > LETHAL > INSCRIBED > FREE
-      for (int i = 0; i < footprint_cells.size(); ++i)
+      unsigned char cost = costmap_2d->getCost(footprint_cells[i].x, footprint_cells[i].y);
+      switch (cost)
       {
-        unsigned char cost = costmap->getCostmap()->getCost(footprint_cells[i].x, footprint_cells[i].y);
-        switch (cost)
-        {
-          case costmap_2d::NO_INFORMATION:
-            response.state = std::max(response.state, static_cast<uint8_t>(mbf_msgs::CheckPose::Response::UNKNOWN));
-            response.cost += cost * (request.unknown_cost_mult ? request.unknown_cost_mult : 1.0);
-            break;
-          case costmap_2d::LETHAL_OBSTACLE:
-            response.state = std::max(response.state, static_cast<uint8_t>(mbf_msgs::CheckPose::Response::LETHAL));
-            response.cost += cost * (request.lethal_cost_mult ? request.lethal_cost_mult : 1.0);
-            break;
-          case costmap_2d::INSCRIBED_INFLATED_OBSTACLE:
-            response.state = std::max(response.state, static_cast<uint8_t>(mbf_msgs::CheckPose::Response::INSCRIBED));
-            response.cost += cost * (request.inscrib_cost_mult ? request.inscrib_cost_mult : 1.0);
-            break;
-          default:
-            response.cost += cost;
-            break;
-        }
+        case costmap_2d::NO_INFORMATION:
+          response.state = std::max(response.state, static_cast<uint8_t>(mbf_msgs::CheckPose::Response::UNKNOWN));
+          response.cost += cost * (request.unknown_cost_mult ? request.unknown_cost_mult : 1.0);
+          break;
+        case costmap_2d::LETHAL_OBSTACLE:
+          response.state = std::max(response.state, static_cast<uint8_t>(mbf_msgs::CheckPose::Response::LETHAL));
+          response.cost += cost * (request.lethal_cost_mult ? request.lethal_cost_mult : 1.0);
+          break;
+        case costmap_2d::INSCRIBED_INFLATED_OBSTACLE:
+          response.state = std::max(response.state, static_cast<uint8_t>(mbf_msgs::CheckPose::Response::INSCRIBED));
+          response.cost += cost * (request.inscrib_cost_mult ? request.inscrib_cost_mult : 1.0);
+          break;
+        default:
+          response.cost += cost;
+          break;
       }
     }
-
-    // Provide some details of the outcome
-    switch (response.state)
-    {
-      case mbf_msgs::CheckPose::Response::OUTSIDE:
-        ROS_DEBUG_STREAM("Pose [" << x << ", " << y << ", " << yaw << "] is outside the map (cost = " << response.cost
-                                  << "; safety distance = " << request.safety_dist << ")");
-        break;
-      case mbf_msgs::CheckPose::Response::UNKNOWN:
-        ROS_DEBUG_STREAM("Pose [" << x << ", " << y << ", " << yaw << "] is in unknown space! (cost = " << response.cost
-                                  << "; safety distance = " << request.safety_dist << ")");
-        break;
-      case mbf_msgs::CheckPose::Response::LETHAL:
-        ROS_DEBUG_STREAM("Pose [" << x << ", " << y << ", " << yaw << "] is in collision! (cost = " << response.cost
-                                  << "; safety distance = " << request.safety_dist << ")");
-        break;
-      case mbf_msgs::CheckPose::Response::INSCRIBED:
-        ROS_DEBUG_STREAM("Pose [" << x << ", " << y << ", " << yaw << "] is near an obstacle (cost = " << response.cost
-                                  << "; safety distance = " << request.safety_dist << ")");
-        break;
-      case mbf_msgs::CheckPose::Response::FREE:
-        ROS_DEBUG_STREAM("Pose [" << x << ", " << y << ", " << yaw << "] is free (cost = " << response.cost
-                                  << "; safety distance = " << request.safety_dist << ")");
-        break;
-    }
-
-    // costmap->checkDeactivate();
-    return true;
   }
 
+  // Provide some details of the outcome
+  switch (response.state)
+  {
+    case mbf_msgs::CheckPose::Response::OUTSIDE:
+      ROS_DEBUG_STREAM("Pose [" << x << ", " << y << ", " << yaw << "] is outside the map (cost = " << response.cost
+                                << "; safety distance = " << request.safety_dist << ")");
+      break;
+    case mbf_msgs::CheckPose::Response::UNKNOWN:
+      ROS_DEBUG_STREAM("Pose [" << x << ", " << y << ", " << yaw << "] is in unknown space! (cost = " << response.cost
+                                << "; safety distance = " << request.safety_dist << ")");
+      break;
+    case mbf_msgs::CheckPose::Response::LETHAL:
+      ROS_DEBUG_STREAM("Pose [" << x << ", " << y << ", " << yaw << "] is in collision! (cost = " << response.cost
+                                << "; safety distance = " << request.safety_dist << ")");
+      break;
+    case mbf_msgs::CheckPose::Response::INSCRIBED:
+      ROS_DEBUG_STREAM("Pose [" << x << ", " << y << ", " << yaw << "] is near an obstacle (cost = " << response.cost
+                                << "; safety distance = " << request.safety_dist << ")");
+      break;
+    case mbf_msgs::CheckPose::Response::FREE:
+      ROS_DEBUG_STREAM("Pose [" << x << ", " << y << ", " << yaw << "] is free (cost = " << response.cost
+                                << "; safety distance = " << request.safety_dist << ")");
+      break;
+  }
+
+  return true;
 }
+
+bool CheckPose::check_pose_cost(costmap_2d::Costmap2DROS* costmap, mbf_msgs::CheckPose::Request& request,
+                                mbf_msgs::CheckPose::Response& response)
+{
+  // ensure costmap is active so cost reflects latest sensor readings
+  // costmap->checkActivate();
+
+  // pad raw footprint to the requested safety distance; note that we discard footprint_padding parameter effect
+  std::vector<geometry_msgs::Point> footprint = costmap->getUnpaddedRobotFootprint();
+  auto costmap_2d = costmap->getCostmap();
+  return check_pose_cost_from_costmap_and_footprint(costmap_2d, footprint, request, response);
+}
+
+}  // namespace mbf_exposed_services
